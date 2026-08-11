db16b2d593dfe8d94a50be9354672912bc128bcb
Bryant Uwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2026-08-11 10:18:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const C='ngplus-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).catch(()=>{}));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(C).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return r;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
+});
