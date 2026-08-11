a706580efcea1d6891b2761d65e94f5e92c1994c
Bryant Uwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2026-08-11 14:39:25 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ngplus-v5';
+const C='ngplus-v8';
 const FILES=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
